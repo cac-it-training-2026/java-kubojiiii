ecd7c53848046ca74b6069e7d5e5b91aaf3bf504
kubotera <kubotera@C010423232.zipangu.cac.co.jp>
2026-04-16 13:58:00 +0900

    Question6_1完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
@@ -6,7 +6,15 @@ public class Question06_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		String[] messages = new String[4];
+
+		messages[0] = "こんにちは";
+		messages[1] = "java学習中";
+		messages[2] = "繰り返しの演習";
+		messages[3] = "頑張ります";
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println(messages[i]);
+		}
 	}
 }
